8c6e6ad53fee4bf761c3c0206bcc15e3e7451161
Marzll <mariiamarqs@gmail.com>
2024-10-23 15:10:17 -0300

    first commit

---
+++ src/app/exibe-resultado/exibe-resultado.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CombustivelService } from '../combustivel.service';
+
+@Component({
+  selector: 'app-exibe-resultado',
+  templateUrl: './exibe-resultado.component.html',
+  styleUrl: './exibe-resultado.component.css'
+})
+export class ExibeResultadoComponent {
+  constructor(public cs: CombustivelService){}
+}
